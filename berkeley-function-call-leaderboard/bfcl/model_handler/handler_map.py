b4bb9a86cf8865426fab258f6afdb871b25a7948
tasneemmidhat10 <129545742+tasneemmidhat10@users.noreply.github.com>
2024-11-20 09:48:01 +0300

    Update handler_map.py

--- berkeley-function-call-leaderboard/bfcl/model_handler/handler_map.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/handler_map.py
@@ -87,6 +87,7 @@ api_inference_handler_map = {
 
 # Inference through local hosting
 local_inference_handler_map = {
+    "Tasneem10/llama-3.2-1b-instruct-fc": customHandler,
     "Tasneem10/Llama3.2-1B-instruct-v1-fc": customHandler,
     "Tasneem10/Llama3.2-1B-instruct-fc": customHandler,
     "google/gemma-2-2b-it": GemmaHandler,
